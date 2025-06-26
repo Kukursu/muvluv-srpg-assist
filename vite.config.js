4cf8ef03136a035d30bf3c37e0427910acc0531d
Kukursu <jeanne3331@gmail.com>
2025-06-26 16:10:23 +0900

    fix: vite.config.jsからinput指定を削除（白画面修正）

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig({
   plugins: [react()],
-  root: '.', // プロジェクト直下に合わせる
   publicDir: 'public',
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: 'public/index.html'
-    }
-  },
+  }
 });

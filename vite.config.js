ec28036f30335925f1cbfb265a48ac97b0644dad
Kukursu <jeanne3331@gmail.com>
2025-06-26 17:13:22 +0900

    fix: vite.config.jsでrootとpublicDirを指定

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: 'src', // ← エントリを src/ にする
-  publicDir: '../public', // ← public フォルダのパスを正しく認識させる
+  root: 'src',
+  publicDir: '../public',
+  base: './',
   plugins: [react()],
   build: {
-    outDir: '../dist', // ← dist をプロジェクト直下に置く
-    emptyOutDir: true,  // ← 古いビルドをクリア（おすすめ）
+    outDir: '../dist',
+    emptyOutDir: true,
   },
 });
+

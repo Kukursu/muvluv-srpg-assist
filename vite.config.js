1ae5e47abbdb7544b4f98a331a73eef76facba4e
Kukursu <jeanne3331@gmail.com>
2025-06-26 15:53:03 +0900

    fix: Viteのエントリーポイント指定追加（RollupError修正）

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  root: '.', // プロジェクト直下に合わせる
+  publicDir: 'public',
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: 'public/index.html'
+    }
   },
-  server: {
-    port: 3000,
-  }
 });

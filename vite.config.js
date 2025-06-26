36cfd7cbe3924e9e75799f93c9b6f3dfa76c40ce
Kukursu <jeanne3331@gmail.com>
2025-06-26 17:16:14 +0900

    fix: main.jsx 修正（React描画開始）

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: 'src',
-  publicDir: '../public',
-  base: './',
   plugins: [react()],
+  publicDir: 'public',
   build: {
-    outDir: '../dist',
-    emptyOutDir: true,
+    outDir: 'dist',
   },
 });
-

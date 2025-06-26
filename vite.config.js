b570516edf3b82fc03a08c6ab52eed7bf3a3e6b0
Kukursu <jeanne3331@gmail.com>
2025-06-26 17:18:11 +0900

    fix: entry index.html resolve for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'public',
+  publicDir: 'public',       // ✅ public/index.html を読む
   build: {
-    outDir: 'dist',
-  },
+    outDir: 'dist'           // ✅ Vercelがこれを使ってデプロイする
+  }
 });
